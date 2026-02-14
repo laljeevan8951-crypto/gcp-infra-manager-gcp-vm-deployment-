4a8e6da004b7f35871f95a6c87f5894dea2b2c34
laljeevan8951-crypto <laljeevan8951@gmail.com>
2026-02-14 12:49:53 +0000

    Create variables.tf

---
+++ infra/variables.tf
@@ -0,0 +1,40 @@
+variable "project_id" {
+  type        = string
+  description = "GCP project ID"
+}
+
+variable "region" {
+  type        = string
+  default     = "europe-west2"
+  description = "GCP region"
+}
+
+variable "zone" {
+  type        = string
+  default     = "europe-west2-a"
+  description = "GCP zone"
+}
+
+variable "name_prefix" {
+  type        = string
+  default     = "imtest"
+  description = "Prefix for resource names"
+}
+
+variable "env" {
+  type        = string
+  default     = "dev"
+  description = "Environment label"
+}
+
+variable "vm_count" {
+  type        = number
+  default     = 3
+  description = "Number of VMs"
+}
+
+variable "machine_type" {
+  type        = string
+  default     = "e2-medium"
+  description = "Compute Engine machine type"
+}
