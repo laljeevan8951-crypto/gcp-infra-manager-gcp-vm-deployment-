2e7ca1c86bd42bec6f10e594a645be18054b9ec3
laljeevan8951-crypto <laljeevan8951@gmail.com>
2026-02-14 12:48:51 +0000

    Create main.tf

---
+++ infra/main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_network" "vpc" {
+  name                    = "${var.name_prefix}-vpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "${var.name_prefix}-subnet"
+  region        = var.region
+  network       = google_compute_network.vpc.id
+  ip_cidr_range = "10.10.0.0/24"
+}
+
+resource "google_compute_instance" "vm" {
+  count        = var.vm_count
+  name         = "${var.name_prefix}-vm-${format("%02d", count.index + 1)}"
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+      size  = 20
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.subnet.id
+    access_config {}
+  }
+
+  labels = {
+    env = var.env
+    app = var.name_prefix
+  }
+}
